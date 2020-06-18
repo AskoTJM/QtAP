c7dc033537bfae5196a5cd89414528f2c1782154
AskoTJM <60477591+AskoTJM@users.noreply.github.com>
2020-06-18 17:08:50 +0300

    addressbook JS stuff
    Now that preliminary JSON works, some cleaning and moving code before continuing.

--- addressbook.qml
+++ addressbook.qml
@@ -20,7 +20,7 @@ StackView {
 
     property alias msg: ttext
     // property will store data
-     property var jsonData: null
+     property var jsonData
 
         Text { id: ttext; anchors.fill: parent; anchors.margins: 10 }
 
@@ -112,8 +112,7 @@ StackView {
         // Component can only have one child, so wrapping everything in Item works around that, grandchildren  > children ?
             Item{
                // Let's try something else,
-                Component.onCompleted: getData2()
-               // Component.onCompleted: Utils.makeRequest()
+                Component.onCompleted:  Utils.getDataFromHeroku()
 
                 Rectangle{
 
@@ -232,37 +231,38 @@ StackView {
 //    }
 
 
-    function getData2(){
+//    function getDataFromHeroku(){
 
-        console.log("Hmmm1")
-        var xhr = new XMLHttpRequest
-            console.log("Hmmm2")
-            xhr.onreadystatechange = function() {
-              if (xhr.readyState === XMLHttpRequest.DONE) {
-                console.log("Hmmm3")
-            // New test code
-                jsonData = xhr.responseText
-                var objectArray = JSON.parse(xhr.responseText)
-            // chug data to safety
-                jsonData = objectArray
-                //console.log("Hmmm4 "+ objectArray.email)
-                  for (var x in objectArray) {
-                      var jsonObject = objectArray[x]
-                      //console.log("Very hmm " + x + " " + jsonObject["email"]) // Object.keys(objectArray).length)
-                      console.log("Very hmm " + x + " " + jsonData["lastname"])
-                  }
-                //
-                var dataString = xhr.responseText
-                //console.log("Hmmm4 "+ dataString)
+//        console.log("Hmmm1")
+//        var xhr = new XMLHttpRequest
+//            console.log("Hmmm2")
+//            xhr.onreadystatechange = function() {
+//              if (xhr.readyState === XMLHttpRequest.DONE) {
+//                console.log("Hmmm3")
+//            // New test code
+//                jsonData = xhr.responseText
+//                var objectArray = JSON.parse(xhr.responseText)
+//            // chug data to safety
+//                abStack.jsonData = objectArray
+//                //console.log("Hmmm4 "+ objectArray.email)
+//                  for (var x in objectArray) {
+//                      //var jsonObject = objectArray[x]
+//                      var jsonObject = abStack.jsonData[x]
+//                      console.log("Very hmm " + x + " " + jsonObject["email"]) // Object.keys(objectArray).length)
+//                      //console.log("Very hmm " + x + " " + abStack.jsonData["email"])
+//                  }
+//                //
+//                var dataString = xhr.responseText
+//                //console.log("Hmmm4 "+ dataString)
 
-                abStack.jsonData = JSON.parse(dataString)
-              }
-            }
-            console.log("Hmmm5")
-            xhr.open("GET", Qt.resolvedUrl("https://qtphone.herokuapp.com/contact"))
-            xhr.send()
-            console.log("Hmmm6")
-          }
+//                abStack.jsonData = JSON.parse(dataString)
+//              }
+//            }
+//            console.log("Hmmm5")
+//            xhr.open("GET", Qt.resolvedUrl("https://qtphone.herokuapp.com/contact"))
+//            xhr.send()
+//            console.log("Hmmm6")
+//          }
 
 
 }
--- littlehelper.js
+++ littlehelper.js
@@ -1,81 +1,34 @@
-/****************************************************************************
-**
-** Copyright (C) 2017 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the examples of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
 
-function showRequestInfo(text) {
-        msg.text = msg.text + "\n" + text
-}
+function getDataFromHeroku(){
 
-function makeRequest()
-{
+    console.log("Hmmm1")
+    var xhr = new XMLHttpRequest
+        console.log("Hmmm2")
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log("Hmmm3")
+        // New test code
+            jsonData = xhr.responseText
+            var objectArray = JSON.parse(xhr.responseText)
+        // chug data to safety
+            abStack.jsonData = objectArray
+            //console.log("Hmmm4 "+ objectArray.email)
+              for (var x in objectArray) {
+                  //var jsonObject = objectArray[x]
+                  var jsonObject = abStack.jsonData[x]
+                  console.log("Very hmm " + x + " " + jsonObject["email"]) // Object.keys(objectArray).length)
+                  //console.log("Very hmm " + x + " " + abStack.jsonData["email"])
+              }
+            //
+            var dataString = xhr.responseText
+            //console.log("Hmmm4 "+ dataString)
 
-    var doc = new XMLHttpRequest();
-    msg.text = "";
-    doc.onreadystatechange = function() {
-        if (doc.readyState === XMLHttpRequest.HEADERS_RECEIVED) {
-            showRequestInfo("Headers -->");
-            showRequestInfo(doc.getAllResponseHeaders ());
-            showRequestInfo("Last modified -->");
-            showRequestInfo(doc.getResponseHeader ("Last-Modified"));
-
-        } else if (doc.readyState === XMLHttpRequest.DONE) {
-            var a = doc.responseXML.documentElement;
-            for (var ii = 0; ii < a.childNodes.length; ++ii) {
-                showRequestInfo(a.childNodes[ii].nodeName);
-            }
-            showRequestInfo("Headers -->");
-            showRequestInfo(doc.getAllResponseHeaders ());
-            showRequestInfo("Last modified -->");
-            showRequestInfo(doc.getResponseHeader ("Last-Modified"));
+            abStack.jsonData = JSON.parse(dataString)
+          }
         }
-    }
-
-    doc.open("GET", "data.xml");
-    doc.send();
+        console.log("Hmmm5")
+        xhr.open("GET", Qt.resolvedUrl("https://qtphone.herokuapp.com/contact"))
+        xhr.send()
+        console.log("Hmmm6")
 }
+
